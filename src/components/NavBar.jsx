5601083b7e9d1b5b7a9556ddd6bef21af3d88651
lethanix <louismurguia@proton.me>
2024-07-10 20:02:44 -0600

    Change banner

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import CartWidget from "./CartWidget.jsx";
 function NavBar() {
     return (
         <>
-            <h3>Debugs Shop</h3>
+            <h3>Debug Shop</h3>
             <ul>
                 <li><a href="">Stickers</a></li>
                 <li><a href="">Shirts</a></li>
